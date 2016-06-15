155ed4ba1a1a1bed4f82162b2548f780b152f3d3
Sylvain Hellegouarch <sh@defuze.org>
2016-06-16 01:38:09 +0200

    cleaner travis description

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# This script will build the HTML docs and push
-# them to the appropriate public repository
+# This script will push the HTML docs
+# to the appropriate public repository
 # serving our github pages
 
 set -o errexit ; set -o nounset
 
-# Building the doc locally
-mkdocs build
-
 # Let's push the generated files to the pubic repository
 cd site
 git init
-git config user.email ""
+git config user.email "travis-ci@atomist.com"
 git config user.name "atomist-travisci"
 git add .
 git commit -m "Generated from ${TRAVIS_REPO_SLUG} ${TRAVIS_COMMIT}"
