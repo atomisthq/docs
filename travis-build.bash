6f8c8d9bf9987c9a5d9dfb70379cd6239bea7e7c
David Dooling <david@atomist.com>
2017-12-08 09:41:00 -0600

    Migrate to docs-atomist-com bucket

--- travis-build.bash
+++ travis-build.bash
@@ -44,8 +44,9 @@ function main () {
 
     [[ $TRAVIS_TAG =~ ^[0-9]+\.[0-9]+\.[0-9]+(-(m|rc)\.[0-9]+)?$ ]] || return 0
 
-    if ! s3cmd sync --delete-removed site/ s3://docs.atomist.com/; then
-        err "failed to sync site to s3"
+    local bucket=docs-atomist-com
+    if ! s3cmd sync --delete-removed site/ "s3://$bucket/"; then
+        err "failed to sync site to s3 bucket '$bucket'"
         return 1
     fi
 
