3c90577898a898147fbe26b8b1bb017a0789d8b9
Jessica Kerr <jessitron@atomist.com>
2018-10-12 18:13:09 -0500

    Turn off fonts because of invalid link
    I don't understand what this font thing is doing. Maybe I should
    exclude the link again instead of removing the font-loading?
    
    Revert "Turn off fonts because of invalid link"
    
    This reverts commit ad99d93feeb3109cb224e1e506779f3f49aad148.
    
    Exclude the font link
    
    I don't understand this HTML so I'll leave the template be

--- htmlproof.bash
+++ htmlproof.bash
@@ -6,11 +6,15 @@ function main() {
     # https://github.com/mkdocs/mkdocs/issues/1655
     local INVALID_SKIP_TO_CONTENT_LINKS='/\.\.\/#/'
     
+    local FONT_LOADING_PRECONNECT_FROM_MATERIAL="https://fonts.gstatic.com"
+    
+    local JENKINS_DOES_NOT_ACCEPT_CONNECTIONS_FROM_TRAVIS="/jenkins.io/"
+    
     local i
     for (( i=0; i < 4; i++ )); do
         # the logo img is added by material theme, so ignore it not having alt
         if bundle exec htmlproofer ./site --alt-ignore '/.*\/atomist-logo-horiz-reversed.svg$/' \
-        --url-ignore "/api.github.com/,/jenkins.io/,$INVALID_SKIP_TO_CONTENT_LINKS"
+        --url-ignore "/api.github.com/,$JENKINS_DOES_NOT_ACCEPT_CONNECTIONS_FROM_TRAVIS,$INVALID_SKIP_TO_CONTENT_LINKS,$FONT_LOADING_PRECONNECT_FROM_MATERIAL"
         then
             return 0
         else
