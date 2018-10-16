83e428714655f029d9865daf6bf1900ceb716e81
Jessica Kerr <jessitron@atomist.com>
2018-10-16 16:43:35 -0500

    Update activate_and_serve

--- activate_and_serve.sh
+++ activate_and_serve.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function die(){
-  echo "$1"
-  exit 1
+    echo "$1"
+    exit 1
 }
 
 . ~/.venvs/userdocs/bin/activate || die "virtual env activation failed"
 pip install -r requirements.txt || die "pip install failed"
 mkdocs build --strict || die "mkdocs build failed"
-bundle exec htmlproofer ./site --alt-ignore '/.*\/atomist-logo-horiz-reversed.svg$/' || die "HTMLProofer failed"
+./htmlproof.bash || die "HTMLProofer failed"
 mkdocs serve
